0c9d68dbcf75cd211f2a48c56f5cb4ada2f8541d
Nil Portugués <nil.portugues@atrapalo.com>
2015-12-03 18:07:16 +0100

    WIP

---
+++ src/Checker/Composer/Reader.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace NilPortugues\NamespaceChecker\Checker\Composer;
+
+/**
+ * Class Reader.
+ */
+class Reader
+{
+    const PSR0 = 'psr-0';
+    const PSR4 = 'psr-4';
+
+    const AUTOLOAD = 'autoload';
+
+    const AUTOLOAD_DEV = 'autoload-dev';
+
+    public static function readAutoloader($composerFile)
+    {
+        $composerFile = self::jsonDecode($composerFile);
+
+        $autoloadProd = self::getAutoloadValues($composerFile, self::AUTOLOAD);
+        $autoloadDev = self::getAutoloadValues($composerFile, self::AUTOLOAD_DEV);
+
+        $autoload[self::PSR0] = array_merge(
+            (!empty($autoloadProd[self::PSR0])) ? $autoloadProd[self::PSR0] : [],
+            (!empty($autoloadDev[self::PSR0])) ? $autoloadDev[self::PSR0] : []
+        );
+
+        $autoload[self::PSR4] = array_merge(
+            (!empty($autoloadProd[self::PSR4])) ? $autoloadProd[self::PSR4] : [],
+            (!empty($autoloadDev[self::PSR4])) ? $autoloadDev[self::PSR4] : []
+        );
+
+        print_r($autoload);
+        die();
+    }
+
+    /**
+     * @param array  $composer
+     * @param string $key
+     *
+     * @return array
+     */
+    private static function getAutoloadValues(array $composer, $key)
+    {
+        return (!empty($composer[$key])) ? $composer[$key] : [];
+    }
+
+    /**
+     * @param string $composerFile
+     *
+     * @return string
+     */
+    private static function jsonDecode($composerFile)
+    {
+        $composerFile = str_replace('\\', '\\\\', $composerFile);
+        $composer = json_decode($composerFile, true);
+
+        if (JSON_ERROR_NONE !== json_last_error()) {
+            throw new \RuntimeException(json_last_error_msg());
+        }
+
+        return $composer;
+    }
+}
---
+++ tests/Checker/Composer/ReaderTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace NilPortugues\Tests\NamespaceChecker\Checker\Composer;
+
+use NilPortugues\NamespaceChecker\Checker\Composer\Reader;
+
+/**
+ * Class ReaderTest.
+ */
+class ReaderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function getComposerFile()
+    {
+        return <<<JSON
+{
+	"autoload": {
+		"psr-0": {
+			"": "src/",
+			"NilPortugues\\AnotherPackage": "src/NilPortugues/AnotherPackage"
+		},
+		"psr-4": {
+			"NilPortugues\\Serializer\\Drivers\\Eloquent\\": "src/"
+		}
+	},
+	"autoload-dev": {
+		"psr-0": {
+			"": "tests/",
+			"NilPortugues\\Tests\\AnotherPackage": "tests/NilPortugues/AnotherPackage"
+		},
+		"psr-4": {
+			"NilPortugues\\Tests\\Serializer\\Drivers\\Eloquent\\": "tests/"
+		}
+	}
+}
+JSON;
+    }
+
+    /**
+     *
+     */
+    public function testItReadsPsr0()
+    {
+        $composer = $this->getComposerFile();
+
+        $namespaces = Reader::readAutoloader($composer);
+    }
+
+    /*
+     *
+     */
+//    public function testItReadsPsr4()
+//    {
+//
+//    }
+}
